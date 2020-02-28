5ffb66c1c1dbb9f4380ddbb0fcb8d87bbcfac24f
Lei Qian <Ike@appledeMBP-3.hub>
2020-02-28 16:20:06 +1100

    Add navbar to home page.

--- src/product/AllProducts.js
+++ src/product/AllProducts.js
@@ -1,6 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import data from "../data/products.json";
+import allRates from "../data/exchange_rates.json";
 
 export const AllProducts = () => {
-  return <div>All</div>;
+  const [currentBase, setBase] = useState("AUD");
+  const handleChange = event => {
+    setBase(event.target.value);
+  };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="/">
+          XYZ
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Current base: {currentBase}
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                {allRates?.map((item, i) => (
+                  <a key={i} className="dropdown-item" href="#">
+                    <input
+                      onChange={handleChange}
+                      value={`${item.base}`}
+                      id={item.base}
+                      name="base"
+                      type="radio"
+                      className="mr-2 ml-4"
+                    />
+                    <label className="form-check-label" htmlFor={item.base}>
+                      {item.base}
+                    </label>
+                  </a>
+                ))}
+              </div>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
 };
